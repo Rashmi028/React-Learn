5c078f6a710f966c72586c442306f3920b741b73
Rashmi028 <vermarashmi028@gmail.com>
2024-06-14 23:51:32 +0530

    commit

---
+++ 06currencyConvertor/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='text-3xl bg-orange-500'>My father is a abuser!!!</h1>
+    </>
+  )
+}
+
+export default App
